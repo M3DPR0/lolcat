7e9404f8c5591769d08b1b115263f14a8b5a2740
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-12 13:26:20 +0700

    Create giy.sh

---
+++ giy.sh
@@ -0,0 +1,11 @@
+cowsay -f eyes "MedPro | Here For God" | lolcat   
+echo ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ | lolcat -a -d 7
+figlet -f standard "SOESANTO" | lolcat
+
+echo ---------------------------------- | lolcat -a -d 2
+echo Welcome Soesanto, Please Foccus... | lolcat -a -d 20
+echo ---------------------------------- | lolcat -a -d 2
+
+echo --------------------------- | lolcat -a -d 2
+date | lolcat
+echo --------------------------- | lolcat -a -d 2
